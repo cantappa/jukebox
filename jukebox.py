f7347dd81cd27d21a1b084ab8b936e2f40683d66
Martina Eikel <martina@eikel.org>
2018-06-30 16:40:40 +0200

    python script

---
+++ jukebox.py
@@ -0,0 +1,449 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# This script runs in the following threads:
+# * display thread: 
+#   - prints contents of display_current to the display,
+#   - this thread is also responsible for scrolling the text
+# * control thread:
+#   - this thread controls button presses and reading of RFID tags
+#   - when a button is pressed it also initiates the according actions
+#
+# The following actions are taken when a button is pressed:
+# * play/pause button:
+#   - execute "mpc -q play/pause" as subprocess
+#   - updates contents of display_current to the currently running track
+# * next button:
+#   - execute "mpc -q next" as subprocess
+#   - updates contents of display_current to the currently running track
+# * previous button:
+#   - execute "mpc -q prev" as subprocess
+#   - updates contents of display_current to the currently running track
+# * volume up button:
+#   - executes "mpc -q volume +2" as subprocess
+#   - update the contents of display_current to hold the text "Lauter"
+#     and the current volume
+#   - store previous display contents and timestamp of when to display them again
+# * volume down button:
+#   - executes "mpc -q volume -2" as subprocess
+#   - the rest is analgous to the volume up action
+#
+# Furthermore, immediately after each button press it is checked
+# if the sequences of recently pressed buttons matches one of the
+# predefined "hidden options".
+#
+# Currently, the following hidden options are supported:
+# * prev - play/pause - next: switch to next directory
+# * volume down - volume up - volume down - volume up: disable/enable the display
+#
+
+import RPi.GPIO as GPIO
+from RPLCD import i2c, gpio
+import time
+import subprocess
+from threading import Thread, Lock
+import sys
+
+########################################################################
+# CONFIGURATION
+########################################################################
+
+# available media directories
+media_list = ['tag-01', 'tag-02', 'tag-03']
+media = {}
+media['tag-01'] = 'Bibi Blocksberg'
+media['tag-02'] = u'Benjamin Blümchen'
+media['tag-03'] = 'Kinderlieder'
+
+# current (and initial) start media directory 
+media_current_dir_index = 0
+
+# define GPIO pins of the buttons
+gpio_play_pause=4	# red button
+gpio_prev=27		# green button
+gpio_next=17		# yellow button
+gpio_volume_up=23	# blue button
+gpio_volume_down=24	# white button
+gpio_onoff=7		# power on/off button
+
+# configure buttons
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_onoff, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(gpio_play_pause, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gpio_prev, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gpio_next, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gpio_volume_up, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(gpio_volume_down, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+
+# sleep time after a button press
+button_press_sleep_time=0.3
+
+# initial text at display:
+display_initial = ['', '']
+display_initial[0] = '* * Paulas * * *'
+display_initial[1] = '* * Jukebox  * *'
+
+# create lcd object
+options = {}
+lcd = i2c.CharLCD('PCF8574', 0x3f, port=1, charmap='A00', cols=16, rows=2, expander_params=options)
+
+# display related variables
+display_current = list(display_initial)
+display_current_lock = Lock()
+display_running = True
+display_scrolling_enabled = True
+display_enabled = True
+display_previous = list(display_current) # in case the display temporarily shows
+										 # status information (e.g. volume info)
+										 # this variable holds the contents of
+										 # the display before showing the status
+										 # informations
+display_previous_scrolling_status = display_scrolling_enabled
+
+# lock for accessing the variable lcd
+lcd_lock = Lock()
+
+# list of threads created during execution
+threads = []
+
+short_msg_time = 1500 # how long to short info messages (in ms)
+show_previous_timestamp = -1 # if set to a positive number display the contents of the previous display again
+
+playing = False
+
+# via button sequences specific "hidden" functions can be triggered
+PLAY_PAUSE = 0
+PREV = 1
+NEXT = 2
+VOLUME_DOWN = 3
+VOLUME_UP = 4
+
+# sequence of recently pressed buttons
+button_press_sequence = []
+
+# sequence for switching to next directory
+sequence_next_dir = [PREV, PLAY_PAUSE, NEXT]
+
+# sequence for disabling/enabling display
+sequence_display = [VOLUME_DOWN, VOLUME_UP, VOLUME_DOWN, VOLUME_UP]
+
+########################################################################
+# FUNCTIONS
+########################################################################
+
+# write the framebuffer out to the given LCD
+def write_to_lcd(framebuffer):
+	global display_enabled
+	global lcd
+	global lcd_lock
+	if not display_enabled:
+		return
+
+	num_cols = 16
+	lcd_lock.acquire()
+	try:
+		lcd.home()
+		for row in framebuffer:
+			lcd.write_string(row.ljust(num_cols)[:num_cols])
+			lcd.write_string('\r\n')
+	finally:
+		lcd_lock.release()
+
+
+# enable/disable display scrolling
+def set_display_scrolling(value):
+	global lcd_lock
+	global display_scrolling_enabled
+	display_scrolling_enabled = value
+
+	# for some reason we need to clear the lcd
+	lcd_lock.acquire()
+	try:
+		lcd.clear()
+	finally:
+		lcd_lock.release()
+
+# get current volume
+def get_current_volume():
+	cmd = "mpc | grep 'volume:' | awk -F'%' '{print $1}' | awk -F':' '{print $2}' | tr -d '[:space:]'"
+	return subprocess.check_output(cmd, shell=True)
+
+
+# handle a button press, i.e.:
+# * run the function button_press_callback in a background thread
+# * add the created thread to the global list of threads
+# * sleep for the configured amount of seconds
+def handle_volume_button_press(text, up):
+	global display_short_msg_thread
+	global display_current
+	global display_current_lock
+	global display_previous
+	global show_previous_timestamp
+	global display_previous_scrolling_status
+	global display_scrolling_enabled
+
+	if up:
+		subprocess.Popen(["mpc", "-q", "volume", "+2"])
+	else:
+		subprocess.Popen(["mpc", "-q", "volume", "-2"])
+
+	new_volume=get_current_volume()
+	print('New volume: '+new_volume)
+
+	if not new_volume:
+		return
+
+	# update display contents to show volume info
+	display_current_lock.acquire()
+	try:
+		# only update display_previous if a short message is currently not shown
+		if show_previous_timestamp == -1:
+			display_previous = list(display_current) # need to copy the list in display_current
+			display_previous_scrolling_status = display_scrolling_enabled
+		display_current[0] = text
+		display_current[1] = u'Lautstärke: '+str(new_volume)+'%'
+		show_previous_timestamp = short_msg_time + int(round(time.time() * 1000))
+	finally:
+		display_current_lock.release()
+
+	# disable scrolling
+	set_display_scrolling(False)
+
+
+# check whether to show the previous display contents again
+def check_and_show_previous():
+	global show_previous_timestamp
+	global display_current
+	global display_current_lock
+	global display_previous
+	global display_previous_scrolling_status
+
+	current = int(round(time.time() * 1000))
+	if show_previous_timestamp >= 0 and current >= show_previous_timestamp:
+		display_current_lock.acquire()
+		try:
+			display_current = list(display_previous)
+		finally:
+			display_current_lock.release()
+		show_previous_timestamp = -1
+		set_display_scrolling(display_previous_scrolling_status)
+
+
+# update the current contents of the LCD with the currently playing track
+def display_thread_callback():
+	global display_running
+	global display_current
+	global display_current_lock
+	global show_previous_timestamp
+
+	display_framebuffer = ['','']
+	
+	while display_running:
+
+		check_and_show_previous()
+		if not display_scrolling_enabled:
+			write_to_lcd(display_current)
+			continue
+		
+		# scroll the second row of the LCD
+		for i in range(len(display_current[1])-16+1):
+
+			# check whether to show the previous display contents again
+			check_and_show_previous()
+
+			# note that the scrolling may be disabled while it is running
+			if not display_scrolling_enabled:
+				write_to_lcd(display_current)
+				continue
+
+			display_current_lock.acquire()
+			display_framebuffer[0] = display_current[0]
+			display_framebuffer[1] = display_current[1][i:i+16]
+			display_current_lock.release()
+			write_to_lcd(display_framebuffer)
+			time.sleep(0.2)
+
+
+# update the contents of display_current to currently running track
+def update_display_current(display_title):
+	global display_current_lock
+
+	title = ''
+	if display_title:
+		cmd="mpc current | head -1"
+		title=subprocess.check_output(cmd, shell=True)
+
+	# if no track is selected do not change the display contents
+	if not title:
+		return
+
+	# get information about current track
+	directory = media[media_list[media_current_dir_index]]
+
+	# update display_current
+	display_current_lock.acquire()
+	try:
+		display_current[0] = directory
+		display_current[1] = title
+	finally:
+		display_current_lock.release()
+
+# print the given sequence of button presses
+def print_button_press_sequence(sequence):
+	sequence_str = ''
+	for elem in sequence:
+		if len(sequence_str) > 0:
+			sequence_str += ','
+		if elem == PLAY_PAUSE:
+			sequence_str += 'PLAY_PAUSE'
+		if elem == NEXT:
+			sequence_str += 'NEXT'
+		if elem == PREV:
+			sequence_str += 'PREV'
+		if elem == VOLUME_UP:
+			sequence_str += 'VOLUME_UP'
+		if elem == VOLUME_DOWN:
+			sequence_str += 'VOLUME_DOWN'
+	print(sequence_str)
+
+
+# compares the last elements of the first list with all elements of the second list,
+# returns true if all compared elements equal
+def sequences_match(long_list, short_list):
+	if len(long_list) < len(short_list):
+		return False
+
+	for i in range(len(short_list)):
+		if short_list[len(short_list)-1-i] != long_list[len(long_list)-1-i]:
+			return False
+	return True
+
+
+# check if currently pressed button sequence matches any of the predefined sequences
+def matching_sequence_found():
+	global display_enabled
+	global media_current_dir_index
+	if sequences_match(button_press_sequence, sequence_next_dir):
+		print('Sequence found: change to next directory')
+		button_press_sequence[:] = []
+		media_current_dir_index = (media_current_dir_index + 1) % len(media_list)
+		subprocess.Popen(["mpc", "stop"])
+		playing = False
+		subprocess.Popen(["mpc", "clear"])
+		subprocess.Popen(["mpc", "update"])
+		subprocess.Popen(["mpc", "add", media_list[media_current_dir_index]])
+		update_display_current(False)
+		return True
+
+	if sequences_match(button_press_sequence, sequence_display):
+		print('Sequence found: enable/disable display')
+		button_press_sequence[:] = []
+		if display_enabled:
+			# disable display
+			display_enabled = False
+			lcd_lock.acquire()
+			try:
+				lcd.display_enabled = False
+				lcd.backlight_enabled = False
+			finally:
+				lcd_lock.release()
+		else:
+			display_enabled = True
+			# enable display
+			lcd_lock.acquire()
+			try:
+				lcd.display_enabled = True
+				lcd.backlight_enabled = True
+			finally:
+				lcd_lock.release()
+		return True
+	return False
+
+def shutdown():
+	display_enabled = False
+	lcd_lock.acquire()
+	try:
+		lcd.display_enabled = False
+		lcd.backlight_enabled = False
+	finally:
+		lcd_lock.release()
+	
+
+########################################################################
+# MAIN
+########################################################################
+
+# write initial text to display
+display_thread = Thread(target=display_thread_callback)
+display_thread.start()
+threads += [display_thread]
+
+# initialize audio player
+subprocess.Popen(["mpc", "clear"])
+subprocess.Popen(["mpc", "update"])
+subprocess.Popen(["mpc", "add", media_list[media_current_dir_index]])
+subprocess.Popen(["mpc", "repeat", "on"])
+
+while True:
+	if GPIO.input(gpio_onoff) == False:
+		print('On/Off pressed')
+		shutdown()
+
+	if GPIO.input(gpio_play_pause) == True:
+		button_press_sequence += [PLAY_PAUSE]
+		if matching_sequence_found():
+			continue
+		if playing:
+			print('Pause')
+			subprocess.Popen(["mpc", "-q", "pause"])
+			playing = False
+			set_display_scrolling(False)
+		else:
+			print('Play')
+			subprocess.Popen(["mpc", "-q", "play"])
+			playing = True
+			set_display_scrolling(True)
+
+		update_display_current(True)
+		time.sleep(button_press_sleep_time)
+
+	elif GPIO.input(gpio_next) == True:
+		print(u'Vorwärts')
+		button_press_sequence += [NEXT]
+		if matching_sequence_found():
+			continue
+		subprocess.Popen(["mpc", "-q", "next"])
+		update_display_current(True)
+		time.sleep(button_press_sleep_time)
+
+	elif GPIO.input(gpio_prev) == True:
+		print(u'Rückwärts')
+		button_press_sequence += [PREV]
+		if matching_sequence_found():
+			continue
+		subprocess.Popen(["mpc", "-q", "prev"])
+		update_display_current(True)
+		time.sleep(button_press_sleep_time)
+
+	elif GPIO.input(gpio_volume_up) == True:
+		print('Lauter')
+		button_press_sequence += [VOLUME_UP]
+		if matching_sequence_found():
+			continue
+		handle_volume_button_press('Lauter', True)
+		time.sleep(button_press_sleep_time)
+
+	elif GPIO.input(gpio_volume_down) == True:
+		print('Leiser')
+		button_press_sequence += [VOLUME_DOWN]
+		if matching_sequence_found():
+			continue
+		handle_volume_button_press('Leiser', False)
+		time.sleep(button_press_sleep_time)
+
+# join all previously started threads
+display_running = False
+for thread in threads:
+	thread.join()
+
+GPIO.cleanup()
