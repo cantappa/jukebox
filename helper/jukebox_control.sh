80a023c0ec9541633dbbe552ff2c4c3b1c61b09e
Martina Eikel <martina@eikel.org>
2019-05-26 13:34:28 +0200

    restructure folder file structure

--- helper/jukebox_control.sh
+++ helper/jukebox_control.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Start/stop/restart Jukebox service.
+# Start/stop/restart/show info of jukebox service.
 # Expects operation to execute as parameter.
 #
 
@@ -11,8 +11,13 @@ elif [ "$1" = "stop" ]; then
         echo "Stopping Jukebox..."
 elif [ "$1" = "restart" ]; then
         echo "Restarting Jukebox..."
+elif [ "$1" = "status" ]; then
+        echo "Jukebox status:"
+elif [ "$1" = "ip" ]; then
+        echo "IP Adresse: "$(ip addr show wlan0 | grep -Po 'inet \K[\d.]+')
+        exit 0
 else
-        echo "Invalid operation. Valid operations: start, restart, stop"
+        echo "Invalid operation. Valid operations: start, restart, stop, status, ip"
         exit 1
 fi
 
